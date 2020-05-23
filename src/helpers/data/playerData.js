569190dc65c78a3881d6f4a56b26ab05baf36088
Michele Rawlins <shellbop@att.net>
2020-05-23 08:48:36 -0500

    playerjs and playerData

--- src/helpers/data/playerData.js
+++ src/helpers/data/playerData.js
@@ -4,5 +4,20 @@ import apiKeys from '../apiKeys.json';
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const getPlayersByTeamId = (teamId) => new Promise((resolve, reject) => {
-  axios.get(`{baseUrl}/players.json?orderBy=`)
-}
+  axios.get(`{baseUrl}/players.json?orderBy="teamId"&equalTo="${teamId}"`)
+    .then((result) => {
+      const allPlayersObject = result.data;
+      const players = [];
+      if (allPlayersObject !== null) {
+        Object.keys(allPlayersObject).forEach((playerId) => {
+          const newPlayer = allPlayersObject[playerId];
+          newPlayer.is = playerId;
+          players.push(newPlayer);
+        });
+      }
+      resolve(players);
+    })
+    .catch((err) => reject(err));
+});
+
+export default getPlayersByTeamId
